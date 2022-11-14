0b33b8687cc705a0ce4c6223e7038ebf6aa332d6
Wander S. Moschetta <wander@familiaprever.com.br>
2022-11-14 16:45:05 -0300

    Incluso background color Uniprev

--- app/src/main/java/br/com/familiaprever/uniprevconecta/ui/theme/Color.kt
+++ app/src/main/java/br/com/familiaprever/uniprevconecta/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package br.com.familiaprever.uniprevconecta.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
+val Purple200 = Color(0xFF5A0F1E)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val BackgroundUniprev = Color(0xFF5A0F1E)
